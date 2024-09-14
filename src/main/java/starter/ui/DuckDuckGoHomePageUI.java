bacb3787d78c1794f68184bb4a53d4b945b2ee35
Dennis SVA <danfesic038@gmail.com>
2024-09-13 21:37:02 -0500

    Directories deleted!

--- src/main/java/starter/ui/DuckDuckGoHomePageUI.java
+++
@@ -1,7 +0,0 @@
-package starter.ui;
-
-import net.serenitybdd.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-@DefaultUrl("page:duckduckgo.base.url")
-public class DuckDuckGoHomePageUI extends PageObject {}
